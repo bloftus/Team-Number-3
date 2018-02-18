e38f21bda1b537f105edde52ebdd0f7a8060046c
Brian Loftus <bloftus@hawkmail.hccfl.edu>
2018-02-18 18:52:02 -0500

    Commiting what I have so far so others can look at it.
    Hey team,
    I have created a MainView but there is an issue with the imports. I'm
    going to keep looking at it but I wanted to push it so you guys could
    see where I'm at.

---
+++ src/main/java/TeamNumber3/MainView.java
@@ -0,0 +1,49 @@
+package TeamNumber3;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class MainView {
+
+	protected Shell shell;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			MainView window = new MainView();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText("SWT Application");
+
+	}
+
+}
