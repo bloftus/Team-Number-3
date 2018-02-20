5fb9dcd454232410351caf615415315acc4f091f
Brian Loftus <bloftus@hawkmail.hccfl.edu>
2018-02-19 23:44:25 -0500

    Added the JUnit test and Expanded the pom.xml
    I used the "Add New JUnit Case" button to create a new test class.
    
    I modified the pom adding some required stuff like changing the
    compilier to 1.8 and adding the junit dependency. I copied from the last
    projects pom.

---
+++ src/test/java/TeamNumber3/MainViewTest.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package TeamNumber3;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Brian
+ *
+ */
+public class MainViewTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link TeamNumber3.MainView#main(java.lang.String[])}.
+	 */
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link TeamNumber3.MainView#MainView()}.
+	 */
+	@Test
+	public void testMainView() {
+		fail("Not yet implemented");
+	}
+
+}
