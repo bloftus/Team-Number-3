4794065eed0834dc0fa931de474f302675cb9307
Brian Loftus <bloftus@hawkmail.hccfl.edu>
2018-02-19 22:10:27 -0500

    Added Title bar to admin view.

--- src/main/java/TeamNumber3/AdminView.java
+++ src/main/java/TeamNumber3/AdminView.java
@@ -1,31 +1,21 @@
 package TeamNumber3;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import java.awt.Component;
-import javax.swing.Box;
-import java.awt.BorderLayout;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
-import java.awt.Font;
 import java.awt.Color;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTable;
-import javax.swing.border.SoftBevelBorder;
-import javax.swing.border.BevelBorder;
-import javax.swing.border.LineBorder;
-import javax.swing.JButton;
+import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableModel;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class AdminView {
 
-	private JFrame frame;
+	private JFrame frmFileSearchSystem;
 	private JLabel txtSearchHeading;
 	private JLabel txtFileName;
 	private JLabel txtStatus;
@@ -40,7 +30,7 @@ public class AdminView {
 			public void run() {
 				try {
 					AdminView window = new AdminView();
-					window.frame.setVisible(true);
+					window.frmFileSearchSystem.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -59,31 +49,32 @@ public class AdminView {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setResizable(false);
-		frame.getContentPane().setBackground(Color.WHITE);
-		frame.setBounds(100, 100, 448, 448);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frmFileSearchSystem = new JFrame();
+		frmFileSearchSystem.setTitle("File Search System");
+		frmFileSearchSystem.setResizable(false);
+		frmFileSearchSystem.getContentPane().setBackground(Color.WHITE);
+		frmFileSearchSystem.setBounds(100, 100, 448, 448);
+		frmFileSearchSystem.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmFileSearchSystem.getContentPane().setLayout(null);
 		
 		txtSearchHeading = new JLabel();
 		txtSearchHeading.setBounds(0, 0, 440, 23);
 		txtSearchHeading.setFont(new Font("Tahoma", Font.BOLD, 14));
 		txtSearchHeading.setHorizontalAlignment(SwingConstants.CENTER);
 		txtSearchHeading.setText("File Search System Administration");
-		frame.getContentPane().add(txtSearchHeading);
+		frmFileSearchSystem.getContentPane().add(txtSearchHeading);
 		
 		txtFileName = new JLabel();
 		txtFileName.setBounds(0, 23, 220, 20);
 		txtFileName.setHorizontalAlignment(SwingConstants.CENTER);
 		txtFileName.setText("File Name");
-		frame.getContentPane().add(txtFileName);
+		frmFileSearchSystem.getContentPane().add(txtFileName);
 		
 		txtStatus = new JLabel();
 		txtStatus.setBounds(220, 23, 220, 20);
 		txtStatus.setHorizontalAlignment(SwingConstants.CENTER);
 		txtStatus.setText("Status");
-		frame.getContentPane().add(txtStatus);
+		frmFileSearchSystem.getContentPane().add(txtStatus);
 		
 		JButton btnRemoveSelectedFiles = new JButton("Remove Selected Files");
 		btnRemoveSelectedFiles.addActionListener(new ActionListener() {
@@ -92,7 +83,7 @@ public class AdminView {
 			}
 		});
 		btnRemoveSelectedFiles.setBounds(253, 344, 151, 23);
-		frame.getContentPane().add(btnRemoveSelectedFiles);
+		frmFileSearchSystem.getContentPane().add(btnRemoveSelectedFiles);
 		
 		JButton btnRebuild = new JButton("Rebuild");
 		btnRebuild.addActionListener(new ActionListener() {
@@ -101,7 +92,7 @@ public class AdminView {
 			}
 		});
 		btnRebuild.setBounds(139, 344, 89, 23);
-		frame.getContentPane().add(btnRebuild);
+		frmFileSearchSystem.getContentPane().add(btnRebuild);
 		
 		JButton btnAddFile = new JButton("Add File...");
 		btnAddFile.addActionListener(new ActionListener() {
@@ -110,7 +101,7 @@ public class AdminView {
 			}
 		});
 		btnAddFile.setBounds(25, 344, 89, 23);
-		frame.getContentPane().add(btnAddFile);
+		frmFileSearchSystem.getContentPane().add(btnAddFile);
 		
 		btnResetWindows = new JButton("Reset Windows");
 		btnResetWindows.addActionListener(new ActionListener() {
@@ -119,16 +110,16 @@ public class AdminView {
 			}
 		});
 		btnResetWindows.setBounds(33, 375, 107, 23);
-		frame.getContentPane().add(btnResetWindows);
+		frmFileSearchSystem.getContentPane().add(btnResetWindows);
 		
 		JLabel lblNumberOfFiles = new JLabel("Number of files indexed: 0");
 		lblNumberOfFiles.setBounds(150, 378, 146, 14);
-		frame.getContentPane().add(lblNumberOfFiles);
+		frmFileSearchSystem.getContentPane().add(lblNumberOfFiles);
 		
 		JLabel lblVersion = new JLabel("version 0.1");
 		lblVersion.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblVersion.setBounds(346, 384, 75, 14);
-		frame.getContentPane().add(lblVersion);
+		frmFileSearchSystem.getContentPane().add(lblVersion);
 		
 		table = new JTable();
 		table.setModel(new DefaultTableModel(
@@ -141,6 +132,6 @@ public class AdminView {
 		table.getColumnModel().getColumn(0).setPreferredWidth(125);
 		table.getColumnModel().getColumn(1).setPreferredWidth(91);
 		table.setBounds(10, 42, 412, 291);
-		frame.getContentPane().add(table);
+		frmFileSearchSystem.getContentPane().add(table);
 	}
 }
