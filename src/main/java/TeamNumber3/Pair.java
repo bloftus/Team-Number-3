809bd4bd653bc011778bffddb8f6c82efc46e5da
Brian Loftus <notlofty@gmail.com>
2018-03-28 23:09:46 -0400

    Adding File via Github
    Can't seem to add a new file to my index in eclipse trying to see what it does when I add one here.

---
+++ src/main/java/TeamNumber3/Pair.java
@@ -0,0 +1,19 @@
+package TeamNumber3;
+
+class Pair {
+	private int fileNum;
+	private int positionNum;
+	
+	public Pair(int fileNum, int positionNum) {
+		this.fileNum = fileNum;
+		this.positionNum = positionNum;
+	}
+	
+	public int getFileNum() {
+		return this.fileNum;
+	}
+	
+	public int getPositionNum() {
+		return this.positionNum;
+	}
+}
