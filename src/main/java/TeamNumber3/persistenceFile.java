0052b8bbb3875d6186092253fb82d6b638214228
Brian Loftus <bloftus@hawkmail.hccfl.edu>
2018-04-05 18:46:40 -0400

    Filled in the constructor for the persistenceFile class
    Trying to move toward being to store data so made a class we can store
    the data we need about files in. I also made the fields private so we
    can use getters and setter methods but I haven't added those yet. And
    I'll change the other classes too to use them. I may use only getters
    and require the use of the constructor with arguments.

--- src/main/java/TeamNumber3/persistenceFile.java
+++ src/main/java/TeamNumber3/persistenceFile.java
@@ -5,12 +5,21 @@ package TeamNumber3;
 import java.io.File;
 
 class persistenceFile {
-	public String filepath;
-	public long dateModified;
-	public int fileNumber;
-
+	private String fileName;
+	private String filepath;
+	private long dateModified;
+	private int fileNumber;
+	public static int highestFileNum;
+	
 	public persistenceFile() {
-
+		
+	}
+	public persistenceFile(String fileName, String filePath, long dateModified) {
+		this.fileName = fileName;
+		this.filepath = filePath;
+		this.dateModified = dateModified;
+		this.fileNumber = highestFileNum + 1;
+		highestFileNum++;
 	}
 
 	public String getFileStatus() {
